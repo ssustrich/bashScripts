c11f7952f995bc70f6ba256fd21288feb5faa1d8
Sustrich <ssustrich@users.noreply.github.com>
2020-02-14 14:01:40 -0800

    Update cleanupDesktop.sh

--- cleanupDesktop.sh
+++ cleanupDesktop.sh
@@ -2,9 +2,24 @@
 imageFileExts="png jpe\?g bmp gif xcf"
 videoFileExts="mp4"
 musicFileExts="mp3"
-documentFileExts="docx\? html\? txt pdf xps pptx\? properties xlsx\? xlsm csv"
+documentFileExts="docx\? html\? txt pdf xps pptx\? properties xlsx\?m\? csv"
 downloadsFileExts="iso zip exe msi 7z gz"
 developmentFileExts="jar java class pyc\? ipynb ear json xml patch jar tar"
+
+ifFileExists () {
+	i=0
+	fileName = $1
+	fname="${fileName%.*}"
+	fext="${fileName##*.}"
+	while [[ -e "../$2/$fname-$i.$fext" ]] ; do
+		let i++
+	done
+	echo "Moving $file to ../$2/$fname.$fext"
+    mv -i $file "../$2/$fname.$fext"
+} 
+
+
+
 for file in *
   do
     echo $file
@@ -12,20 +27,11 @@ for file in *
       do
         if echo $file | grep -qi "\.${ext}$"
           then
-		  if [[ -e "../Pictures/$file" ]] ; then
-			i=0
-			fname="${file%.*}"
-			fext="${file##*.}"
-			while [[ -e "../Pictures/$fname-$i.$fext" ]] ; do
-				let i++
-				echo $i
-			done
-			fname="$fname-$i"
-			echo $fname
-			echo $fext
-		  fi
+		  #if [[ -e "../Pictures/$file" ]] ; then
+		#	ifFileExists "$file" "Pictures"
+		  #fi
 		  echo "Moving $file to ../Pictures/$fname.$fext"
-          mv -i "$file" ../Pictures/$fname.$fext
+          mv -i $file "../Pictures/$fname.$fext"
         fi
       done
 
@@ -33,16 +39,9 @@ for file in *
       do
         if echo $file | grep -qi "\.${ext}$"
 		then
-		  if [[ -e "../Videos/$file" ]] ; then
-			i=0
-			fname="${file%.*}"
-			fext="${file##*.}"
-			while [[ -e "../Videos/$fname-$i.$fext" ]] ; do
-				let i++
-				echo $i
-			done
-			fname="$fname-$i"
-		  fi
+		  #if [[ -e "../Videos/$file" ]] ; then
+		#	ifFileExists "$file" "Videos"
+		  #fi
 		  echo "Moving $file to ../Videos/$fname.$fext"
           mv -i "$file" ../Videos/$fname.$fext
         fi
@@ -52,15 +51,9 @@ for file in *
       do
 	    if echo $file | grep -qi "\.${ext}$"
 		then
-		  if [[ -e "../Music/$file" ]] ; then
-			i=0
-			fname="${file%.*}"
-			fext="${file##*.}"
-			while [[ -e "../Music/$fname-$i.$fext" ]] ; do
-				let i++
-			done
-			fname="$fname-$i"
-		  fi
+		  #if [[ -e "../Music/$file" ]] ; then
+			#ifFileExists "$file" "Music"
+		  #fi
 		  echo "Moving $file to ../Music/$fname.$fext"
           mv -i "$file" ../Music/$fname.$fext
         fi
@@ -71,17 +64,11 @@ for file in *
       do
 	    if echo $file | grep -qi "\.${ext}$"
 		then
-		  if [[ -e "../Documents/$file" ]] ; then
-			i=0
-			fname="${file%.*}"
-			fext="${file##*.}"
-			while [[ -e "../Docuemnts/$fname-$i.$fext" ]] ; do
-				let i++
-			done
-			fname="$fname-$i"
-		  fi
+		  #if [[ -e "../Documents/$file" ]] ; then
+			#ifFileExists "$file" "Documents"
+		  #fi
 		  echo "Moving $file to ../Documents/$fname.$fext"
-          mv -i "$file" ../Documents/$fname.$fext
+          mv -i "$file" "../Documents/$fname.$fext"
         fi
       done
 
@@ -89,35 +76,25 @@ for file in *
       do
         if echo $file | grep -qi "\.${ext}$"
           then
-            if [[ -e "../Downloads/$file" ]] ; then
-			i=0
-			fname="${file%.*}"
-			fext="${file##*.}"
-			while [[ -e "../Dosloads/$fname-$i.$fext" ]] ; do
-				let i++
-			done
-			fname="$fname-$i"
-		  fi
+           # if [[ -e "../Downloads/$file" ]] ; then
+			#	ifFileExists "$file" "Downloads"
+		   # fi
 		  echo "Moving $file to ../Downloads/$fname.$fext"
           mv -i "$file" ../Downloads/$fname.$fext
         fi
       done
 
-    for ext in $developmentFileExts
-      do
-        if echo $file | grep -qi "\.${ext}$"
-          then
-            if [[ -e "/mnt/d/Development/$file" ]] ; then
-			i=0
-			fname="${file%.*}"
-			fext="${file##*.}"
-			while [[ -e "/mnt/d/Development/$fname-$i.$fext" ]] ; do
-				let i++
-			done
-			fname="$fname-$i"
-		  fi
-		  echo "Moving $file to /mnt/d/Development/$fname.$fext"
-          mv -i "$file" /mnt/d/Development/$fname.$fext
-        fi
-      done  
+    #for ext in $developmentFileExts
+    #  do
+    #    if echo $file | grep -qi "\.${ext}$"
+	#	  fname="${file%.*}"
+	#	  fext="${file##*.}"
+    #      then
+    #        if [[ -e "/mnt/d/Development/$file" ]] ; then
+	#			ifDevFileExists "$file" "Development"
+	#	  fi
+	#	  echo "Moving $file to /mnt/d/Development/$fname.$fext"
+    #      mv -i "$file" /mnt/d/Development/$fname.$fext
+    #    fi
+    #  done  
 done
